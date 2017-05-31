2d409ba5006f4857cc2260251e15417efa0e7dc7
mikesmall <michael.l.a.small@gmail.com>
2017-05-31 17:30:44 -0400

    Completed step 05_hello_friend (works)

---
+++ 05_hello_friend/hello_friend.rb
@@ -0,0 +1,19 @@
+class Friend
+
+  # Hint: remember default values?
+  #
+  # def greet(name = ?)
+  #   # your code here
+  # end
+  #
+  # What should ? be equal to?
+
+  def greet(name = "")
+
+    "Hello #{ name }!"
+
+
+
+  end#greet
+
+end#Friend
