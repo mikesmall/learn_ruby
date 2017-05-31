8a97369507492c96553119767975df7d1b62630a
mikesmall <michael.l.a.small@gmail.com>
2017-05-31 16:27:09 -0400

    Completed step 04_pig_latin with errors... Only way to solve is just to copy someone else's pig latin work from StackOverflow etc. Moving onto next step instead.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,19 @@
 def translate(string)
-
-  # Pseudocode:
-
-  # First -- SPLIT string.
+  # Pseudo-code:
+  # First --- SPLIT string.
   # Second -- IF index 0 is A, E, I, O, U, or Y, << "ay", join
-  # Third -- ELSE move 0 to end, then << "ay", join
+  # Third --- ELSE move 0 to end, then << "ay", join
+  string_array = string.split
 
+  if string_array[0] = a || e || i || o || u || y
+    string_array << "ay"
+
+  else
+    string_array << "ay"
+  end#if
   
+  string = string_array.join
 
+  return string
 
 end#def_translate
